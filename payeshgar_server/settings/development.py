41d81e618374b14aca0b5ddca4fa2c3667b7db8f
Mahdi Zareie <mahdi.zareie@pm.me>
2020-07-17 23:54:22 +0430

    Configure logging

--- payeshgar_server/settings/development.py
+++ payeshgar_server/settings/development.py
@@ -1,9 +1,36 @@
 from .common import *
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '[{asctime}] {levelname} {name} {message}',
+            'style': '{',
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+    },
+    'root': {
+        'handlers': [],
+        'level': 'DEBUG',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': "DEBUG",
+            'propagate': False,
+        }
+    },
 }
---
+++ payeshgar_server/settings/production.py
@@ -0,0 +1,31 @@
+from .common import *
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '[{asctime}] {levelname} {name} {message}',
+            'style': '{',
+        }
+    },
+    'handlers': {
+        'syslog': {
+            'class': 'logging.handlers.SysLogHandler',
+            'formatter': 'standard',
+            'facility': 'local0',
+            'address': '/dev/log'
+        }
+    },
+    'root': {
+        'handlers': [],
+        'level': 'DEBUG',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['syslog'],
+            'level': "ERROR",
+            'propagate': False,
+        }
+    },
+}
