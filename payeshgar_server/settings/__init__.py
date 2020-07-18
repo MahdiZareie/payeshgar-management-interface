c3ff7164f0ac3432cb1e32938adb2c6021dcc6be
Mahdi Zareie <mahdi.zareie@pm.me>
2020-07-18 16:23:36 +0430

    Change pagination method to PageNumber

--- payeshgar_server/settings/__init__.py
+++ payeshgar_server/settings/__init__.py
@@ -79,8 +79,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
 
 ENVIRONMENT = os.getenv("PAYESHGAR_ENVIRONMENT", "PRODUCTION")
